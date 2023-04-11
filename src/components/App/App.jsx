9ffc8bd2ebb12586b1e9ead47e262d682fc85f76
Sofia Peshekhonova <Peshekhonova94@yandex.ru>
2023-04-11 16:24:09 +0300

    архитектура и Promo

---
+++ src/components/App/App.jsx
@@ -0,0 +1,31 @@
+import { Routes, Route } from 'react-router-dom';
+// import Register from '../Register/Register';
+// import Login from '../Login/Login';
+import Main from '../Main/Main';
+// import SavedMovies from '../SavedMovies/SavedMovies';
+// import Profile from '../Profile/Profile';
+import "./App.scss";
+
+function App() {
+
+  return (
+    <div className="page">
+        <Routes>
+          {/* <Route path="/signup" element={
+            <Register />
+            }>
+          </Route>
+          <Route path="/signin" element={
+            <Login />
+            }>
+          </Route> */}
+           <Route path="/" element={ <Main />} /> 
+           {/* <Route path="/saved-movies" element={ <SavedMovies />} />
+           <Route path="/profile" element={ <Profile />} /> */}
+        {/* <Route path="*" element={<NotFound/>} />  */}
+        </Routes>
+    </div>
+  );
+}
+
+export default App;
