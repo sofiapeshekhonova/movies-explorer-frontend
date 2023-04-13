2fcbe0ae1112d688c3f386f960674748758bd986
Sofia Peshekhonova <Peshekhonova94@yandex.ru>
2023-04-13 12:16:09 +0300

    level-2: доработки по фильмам

--- src/components/MoviesCard/MoviesCard.jsx
+++ src/components/MoviesCard/MoviesCard.jsx
@@ -5,17 +5,23 @@ function MoviesCard({ film, props }) {
   const { nameRU, trailerLink, duration } = film;
   const {className, src, alt} = props;
 
+  console.log(duration)
+  const hours = Math.trunc(duration / 60);
+  const minutes = Math.trunc(duration - hours * 60);
+
   return (
     <li className="movies-card">
       <a className="movies-card__trailer-link" href={trailerLink} target="_blank" rel="noreferrer">
         <img className="movies-card__image" alt="постер фильма" src={poster}/>
       </a>
         <div className="movies-card__container">
+          <div>
           <div className="movies-card__info">
             <h4 className="movies-card__title">{nameRU}</h4>
             <img className={className} alt={alt} src={src}/>
           </div>
-          <p className="movies-card__time">{duration}</p>
+          <p className="movies-card__time">{hours}ч {minutes}м</p>
+          </div>
       </div>  
     </li>
   );
--- src/components/MoviesCardList/MoviesCardList.jsx
+++ src/components/MoviesCardList/MoviesCardList.jsx
@@ -1,14 +1,46 @@
 import MoviesCard from "../MoviesCard/MoviesCard";
 import "./MoviesCardList.scss";
 import mock from "../../utils/mock";
+import { useEffect, useState } from "react";
 
 function MoviesCardList(props) {
+
+  const [countCard, setCountCard] = useState(12)
+
+  useEffect(() => {
+    let timer;
+    const handleChangeWidthScreenTimer = () => {
+      timer = setTimeout(handleChangeWidthScreen, 300);
+    };
+    window.addEventListener("resize", handleChangeWidthScreenTimer);
+    return () => {
+      window.removeEventListener("resize", handleChangeWidthScreenTimer);
+      clearTimeout(timer);
+    };
+  });  
+
+  const handleChangeWidthScreen = () => {    
+    if (window.innerWidth < 376) {
+      setCountCard(5)
+    } else if (window.innerWidth < 898) {
+      setCountCard(8)
+    } else {
+      setCountCard(12)
+    }
+  }
+
   return (
     <section className="movies-card-list">
       <ul className="movies-card-list__list">
-        {mock.map((film) => (
-          <MoviesCard key={film.id} film={film} props={props}/>
-        ))}
+        {mock.map((film, index) => {
+          if (index < countCard) {
+            return (
+              <MoviesCard key={film.id} film={film} props={props}/>
+            )
+          } else {
+            return ('');
+          }        
+        })}
       </ul>
       <button
         className="movies-card-list__button"
